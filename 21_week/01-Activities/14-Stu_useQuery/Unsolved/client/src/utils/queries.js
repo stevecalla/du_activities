e09279f3f48466a1b9827e392a1aa4c9b07b36bc
Steve Calla <callasteven@gmail.com>
2022-11-08 17:50:06 -0700

    Week 21

--- 21_week/01-Activities/14-Stu_useQuery/Unsolved/client/src/utils/queries.js
+++ 21_week/01-Activities/14-Stu_useQuery/Unsolved/client/src/utils/queries.js
@@ -2,4 +2,12 @@ import { gql } from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql`
   # TODO: Add a query called 'getThoughts' that returns a thought's _id, thoughtText, thoughtAuthor, and createdAt fields
+  query getThoughts {
+    thoughts {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+    }
+  }
 `;
